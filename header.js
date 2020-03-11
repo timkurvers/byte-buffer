ab45f45f1bef3970a6f24792c3cdab262cfc6c99
Tim Kurvers <tim@moonsphere.net>
2020-03-11 17:22:37 +0100

    Update copyright years

--- header.js
+++ header.js
@@ -3,7 +3,7 @@ import header from 'gulp-header';
 export default function(pkg) {
   return header(`/**
  * ${pkg.name} v${pkg.version}
- * Copyright (c) 2012-2015 ${pkg.author}
+ * Copyright (c) 2012-2020 ${pkg.author}
  *
  * ${pkg.description}.
  *
